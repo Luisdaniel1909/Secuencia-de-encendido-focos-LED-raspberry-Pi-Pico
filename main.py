14433af208ac560a17c32fbbe07f80d0b3aa2753
Luis Daniel <96265875+Luisdaniel1909@users.noreply.github.com>
2023-04-04 18:29:02 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import machine
+import utime
+
+# Se definen los pines a utilizar para los LEDs
+led_pins = [2, 3, 4, 5, 6, 7, 8]
+
+# Configurar los pines como salidas
+leds = [machine.Pin(pin, machine.Pin.OUT) for pin in led_pins]
+
+# Se define la duración de cada estado de encendido/apagado de los LEDs en segundos
+state_duration = 0.1
+
+while True:
+    # Enciende el LED del medio
+    leds[3].value(1)
+    utime.sleep(state_duration)
+
+    # Enciende los LEDs de los extremos
+    leds[2].value(1)
+    leds[4].value(1)
+    utime.sleep(state_duration)
+
+    # Enciende los LEDs adyacentes a los de los extremos
+    leds[1].value(1)
+    leds[5].value(1)
+    utime.sleep(state_duration)
+
+    # Enciende los LEDs adyacentes a los del paso anterior
+    leds[0].value(1)
+    leds[6].value(1)
+    utime.sleep(state_duration)
+
+    # Apaga todos los LEDs
+    for led in leds:
+        led.value(0)
+    utime.sleep(state_duration)
